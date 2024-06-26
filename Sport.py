b0ff36d280326aaa288a941ea655606e7e930458
Tom Yordanov <tmy026@icloud.com>
2024-06-26 03:26:25 +0300

    code refactoring

--- Sport.py
+++ Sport.py
@@ -7,8 +7,9 @@ import pyperclip
 import pyautogui
 from datetime import datetime
 
+
 def shouldItStart():
-    """Method to calculate if the program should start"""
+    """Method to check if the program should start"""
     hour_to_start = 08
     mins_to_start = 00
     while True:
@@ -20,6 +21,7 @@ def shouldItStart():
         if hour == hour_to_start and mins == mins_to_start:
             break
 
+
 def wait(seconds):
     """Method wait"""
     time.sleep(seconds)
@@ -30,21 +32,7 @@ def openGoogleMeet():
     webbrowser.open('https://meet.google.com/kbz-jazk-jru')
 
 
-def muteMic(x, y):
-    """Method that moves the cursor position at x, y coordinates and simulates mouse click"""
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-
-
-def stopCamera(x, y):
-    """Method that moves the cursor position at x, y coordinates and simulates mouse click"""
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-
-
-def join(x, y):
+def move_mouse_and_click(x, y):
     """Method that moves the cursor position at x, y coordinates and simulates mouse click"""
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
@@ -53,20 +41,16 @@ def join(x, y):
 
 def writeDownYourInfo(x, y):
     """Method that simulates typing, mouse click and moves the cursor to x,y position"""
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+    move_mouse_and_click(x,y)
     pyperclip.copy('Том Йорданов СИ 2101321049')
-    time.sleep(2)
+    wait(2)
     pyautogui.hotkey("ctrl", "v")
-    time.sleep(1)
+    wait(1)
 
 def sendInfo(x, y):
     """Method that simulates mouse click, moves the cursor to x,y position, waits x time, and closes the browser"""
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-    time.sleep(3300)
+    move_mouse_and_click(x,y)
+    wait(3300)
     pyautogui.hotkey("alt", "f4")
 
 
@@ -75,14 +59,11 @@ def main():
     shouldItStart()
     openGoogleMeet()
     wait(60)
-
-    muteMic(680, 750)
-    stopCamera(770, 750)
+    move_mouse_and_click(680, 750)
+    move_mouse_and_click(770, 750)
     wait(5)
-
-    join(1246, 570)
+    move_mouse_and_click(1246, 570)
     wait(66)
-
     writeDownYourInfo(1780, 1030)
     sendInfo(1856, 960)
 
